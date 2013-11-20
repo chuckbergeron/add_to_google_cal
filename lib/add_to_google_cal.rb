8125f76891b9dc0847534e328e93b78272379262
Charles Bergeron <chuck.bergeron@gmail.com>
2013-11-19 19:19:31 -0800

    Rethinking the URL builder and tests for converting dates

--- lib/add_to_google_cal.rb
+++ lib/add_to_google_cal.rb
@@ -4,17 +4,43 @@ require 'active_record'
 # require 'active_support/core_ext'
 # require File.join(File.dirname(__FILE__), "add_to_google_cal/railtie")
 
-module AddToGoogleCal
+class AddToGoogleCalBuilder
 
-  def gcal_href
-    "https://www.google.com/calendar/render?action=TEMPLATE&dates=20190726T000000Z/20190726T000000Z&sprop=website:http://john-and-marie.craftedtoyou.com&text=John+Dylan+%26+Marie+Skye&location=New+York+City+%40+12%3A00+am&sprop=name:John+Dylan+%26+Marie+Skye&details=Any+notes%2C+descriptions%2C+links%2C+or+reminders+can+be+written+here%21Accomodations%3A+Sudden+Valley+Golf+Resort+%26+Casino%0D%0A%0D%0AGroup+Name%3A+John+Dylan+%26+Marie+Skye+Wedding%0D%0A%0D%0AGroup+Reservation+%23%3A+149602%0D%0A%0D%0ARates%3A+%24149+-+%24179%0D%0A%0D%0ACut-off+date%3A+July+30%2C+2014%0D%0A%0D%0AReservations+line%3A+1.866.555.5555%0D%0A"
+  def initialize(hash)
+    @hash = hash
   end
 
-  # autoload :Hook, File.join(File.dirname(__FILE__), "add_to_google_cal/hook")
-  # autoload :InstanceMethods, File.join(File.dirname(__FILE__), "add_to_google_cal/instance_methods")
+  def call
+    "https://www.google.com/calendar/render?action=TEMPLATE&dates=#{dates}"
+  end
+
+  private
+
+    def dates
+      "#{dtstart.utc.strftime('%Y%m%dT%H%M%SZ')}/#{dtend.utc.strftime('%Y%m%dT%H%M%SZ')}"
+    end
+
+    def dtstart
+      @hash[:dtstart]
+    end
+
+    def dtend
+      @hash[:dtend]
+    end
 
 end
 
+# module AddToGoogleCal
+
+#   def gcal_href
+#     "https://www.google.com/calendar/render?action=TEMPLATE&dates=20190726T000000Z/20190726T000000Z&sprop=website:http://john-and-marie.craftedtoyou.com&text=John+Dylan+%26+Marie+Skye&location=New+York+City+%40+12%3A00+am&sprop=name:John+Dylan+%26+Marie+Skye&details=Any+notes%2C+descriptions%2C+links%2C+or+reminders+can+be+written+here%21Accomodations%3A+Sudden+Valley+Golf+Resort+%26+Casino%0D%0A%0D%0AGroup+Name%3A+John+Dylan+%26+Marie+Skye+Wedding%0D%0A%0D%0AGroup+Reservation+%23%3A+149602%0D%0A%0D%0ARates%3A+%24149+-+%24179%0D%0A%0D%0ACut-off+date%3A+July+30%2C+2014%0D%0A%0D%0AReservations+line%3A+1.866.555.5555%0D%0A"
+#   end
+
+#   # autoload :Hook, File.join(File.dirname(__FILE__), "add_to_google_cal/hook")
+#   # autoload :InstanceMethods, File.join(File.dirname(__FILE__), "add_to_google_cal/instance_methods")
+
+# end
+
 # if Object.const_defined? :ActiveRecord
-  ActiveRecord::Base.send(:include, AddToGoogleCal)
+  # ActiveRecord::Base.send(:include, AddToGoogleCal)
 # end
--- spec/lib/add_to_google_cal_spec.rb
+++ spec/lib/add_to_google_cal_spec.rb
@@ -1,16 +1,34 @@
 require 'spec_helper'
 
-describe AddToGoogleCal do
+describe AddToGoogleCalBuilder do
+
+  describe "dates" do
+    it "generates the dates" do
+      add = AddToGoogleCalBuilder.new(dtstart: Time.utc(2013, 01, 03, 12, 00),
+        dtend: Time.utc(2013, 01, 03, 14, 00))
+      url = add.call
+
+      url.should include("&dates=20130103T120000Z/20130103T140000Z")
+    end
+  end
+
+  describe '#new' do
+
+    it 'expects an object' do
+
+    end
+
+  end
 
   describe "has_google_cal" do
 
-    it "should respond_to gcal_href" do
+    xit "should respond_to gcal_href" do
       expect(Wedding.new).to respond_to?(:gcal_href)
     end
 
     describe "#gcal_href" do
 
-      it "builds the add to google cal link" do
+      xit "builds the add to google cal link" do
         expect(Wedding.new.gcal_href).to match(/google.com/)
       end
 
